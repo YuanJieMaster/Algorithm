b5f9183d228ebeed1bbb7666d9222a878721c461
YuanJieMaster <zzx1179532068@outlook.com>
2025-12-02 14:01:54 +0800

    chore:动态规划 - 不同的二叉搜索树

---
+++ src/DynamicProgramming/Test7/Solution1/Main.java
@@ -0,0 +1,28 @@
+package DynamicProgramming.Test7.Solution1;
+
+/**
+ * 不同的二叉搜索树
+ * 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？
+ * 返回满足题意的二叉搜索树的种数。
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(new Solution().numTrees(n));
+    }
+}
+
+class Solution {
+    public int numTrees(int n) {
+        int[] res = new int[n+1];
+        res[0] = 1;
+        res[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                res[i] += res[j - 1] * res[i - j];
+            }
+        }
+        return res[n];
+    }
+}
