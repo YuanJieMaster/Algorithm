71f1a509753859ec63bf092c62a6e8f67f01fa59
YuanJieMaster <zzx1179532068@outlook.com>
2026-03-31 14:39:00 +0800

    solve:Hot 100-字母异位词分组

---
+++ src/Hot100/Test2/Solution1/Main.java
@@ -0,0 +1,40 @@
+package Hot100.Test2.Solution1;
+
+/**
+ * 字母异位词分组
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * 示例 1:
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * 示例 2:
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ * 示例 3:
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(new Solution().groupAnagrams(strs));
+    }
+}
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] s = str.toCharArray();
+            Arrays.sort(s);
+            String key = String.valueOf(s);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
